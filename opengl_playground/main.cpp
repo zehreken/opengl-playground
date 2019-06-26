502e893dc3a1a750d10c952c97cbe7ff7eb89940
zehreken <zehreken@gmail.com>
2019-06-26 20:20:22 +0200

    Add initial files

---
+++ opengl_playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl_playground
+//
+//  Created by Guchan Alkan on 26.06.2019.
+//  Copyright © 2019 zehreken. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
