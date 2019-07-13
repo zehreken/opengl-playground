e8bc4160a6663b8e68bd5ba707ca78386b6b7ddc
zehreken <zehreken@gmail.com>
2019-07-13 23:29:40 +0200

    Add more cubes

--- opengl_playground/cubes/cubes.cpp
+++ opengl_playground/cubes/cubes.cpp
@@ -3,6 +3,20 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+static glm::vec3 cubePositions[] =
+{
+	glm::vec3(0.0f, 0.0f, 0.0f),
+	glm::vec3(2.0f, 5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f, 3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f, 2.0f, -2.5f),
+	glm::vec3(1.5f, 0.2f, -1.5f),
+	glm::vec3(-1.3f, 1.0f, -1.5f)
+};
+
 Cubes::Cubes()
 {
 	_shader = {"/Users/zehreken/Development/opengl_playground/opengl_playground/cubes/vertex2.txt",
@@ -161,6 +175,14 @@ void Cubes::update()
 	
 	glBindVertexArray(_vao);
 //	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	for (int i = 0; i < 10; i++)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, cubePositions[i]);
+		float angle = 20.0f * i;
+		model = glm::rotate(model, glm::radians(angle + (float)SDL_GetTicks() / 100.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 	glBindVertexArray(0);
 }
