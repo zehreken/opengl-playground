4a6d8b76108ba9f970f01f39cb735e533ccac588
zehreken <zehreken@gmail.com>
2019-08-06 22:30:05 +0200

    Add cube

--- opengl_playground/4_color/color.cpp
+++ opengl_playground/4_color/color.cpp
@@ -49,9 +49,54 @@ Color::Color()
 		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
 		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
 	};
+	
+	glGenVertexArrays(1, &_vao);
+	glBindVertexArray(_vao);
+	
+	glGenBuffers(1, &_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+	glEnable(GL_DEPTH_TEST);
 }
 
 void Color::update()
 {
+	_shader.use();
 	
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians((float)SDL_GetTicks() / 10.0f), glm::vec3(1.0f, 1.0f, 0.0f));
+	
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -(float)SDL_GetTicks() / 1000.0f));
+	
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
+	
+	unsigned int modelLoc = glGetUniformLocation(_shader.ID, "model");
+	unsigned int viewLoc  = glGetUniformLocation(_shader.ID, "view");
+	unsigned int projLoc  = glGetUniformLocation(_shader.ID, "projection");
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	
+	glBindVertexArray(_vao);
+	for (int i = 0; i < 1; i++)
+	{
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0, 0.0, 0.0));
+		float angle = 20.0f * i;
+		model = glm::rotate(model, glm::radians(angle + (float)SDL_GetTicks() / 100.0f), glm::vec3(1.0f, 0.3f, 0.5f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+	glBindVertexArray(0);
 }
