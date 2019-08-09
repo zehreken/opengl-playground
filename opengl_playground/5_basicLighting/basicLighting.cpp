67c21b1452857b6ade05e0724ea4b13d31fb9b80
zehreken <zehreken@gmail.com>
2019-08-10 00:06:24 +0200

    Add diffuse light

--- opengl_playground/5_basicLighting/basicLighting.cpp
+++ opengl_playground/5_basicLighting/basicLighting.cpp
@@ -10,47 +10,47 @@ BasicLighting::BasicLighting()
 	
 	float vertices[] =
 	{
-		-0.5f, -0.5f, -0.5f,
-		0.5f, -0.5f, -0.5f,
-		0.5f, 0.5f, -0.5f,
-		0.5f, 0.5f, -0.5f,
-		-0.5f, 0.5f, -0.5f,
-		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f,
 		
-		-0.5f, -0.5f, 0.5f,
-		0.5f, -0.5f, 0.5f,
-		0.5f, 0.5f, 0.5f,
-		0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
-		-0.5f, -0.5f, 0.5f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
 		
-		-0.5f, 0.5f,  0.5f,
-		-0.5f, 0.5f, -0.5f,
-		-0.5f, -0.5f, -0.5f,
-		-0.5f, -0.5f, -0.5f,
-		-0.5f, -0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f,  0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f,
 		
-		0.5f, 0.5f, 0.5f,
-		0.5f, 0.5f, -0.5f,
-		0.5f, -0.5f, -0.5f,
-		0.5f, -0.5f, -0.5f,
-		0.5f, -0.5f, 0.5f,
-		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
 		
-		-0.5f, -0.5f, -0.5f,
-		0.5f, -0.5f, -0.5f,
-		0.5f, -0.5f, 0.5f,
-		0.5f, -0.5f, 0.5f,
-		-0.5f, -0.5f, 0.5f,
-		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f,
 		
-		-0.5f, 0.5f, -0.5f,
-		0.5f, 0.5f, -0.5f,
-		0.5f, 0.5f, 0.5f,
-		0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, -0.5f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
 	};
 	
 	glGenVertexArrays(1, &_vao);
@@ -60,8 +60,10 @@ BasicLighting::BasicLighting()
 	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -70,7 +72,7 @@ BasicLighting::BasicLighting()
 	glGenVertexArrays(1, &_lightVao);
 	glBindVertexArray(_lightVao);
 	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	
 	glEnable(GL_DEPTH_TEST);
@@ -79,29 +81,37 @@ BasicLighting::BasicLighting()
 static float lr = 0.0f;
 static float lg = 0.0f;
 static float lb = 0.0f;
+static float lightX = -10.0f;
 void BasicLighting::update(int deltaX, int deltaY)
 {
-	lr = sin(SDL_GetTicks() * 0.001f);
-	lg = sin(SDL_GetTicks() * 0.001f);
+	lr = sin(SDL_GetTicks() * 0.001f) + 2.2f;
+	lg = sin(SDL_GetTicks() * 0.001f) + 2.2f;
+	lightX = sin(SDL_GetTicks() * 0.001f) * 15;
 	_camera.update(deltaX, deltaY);
 	_shader.use();
 	unsigned int objectColorLoc = glGetUniformLocation(_shader.ID, "objectColor");
 	glUniform3f(objectColorLoc, 0.5f, 0.5f, 0.5f);
 	unsigned int lightColorLoc = glGetUniformLocation(_shader.ID, "lightColor");
 	glUniform3f(lightColorLoc, lr, lg, lb);
+	unsigned int lightPosLoc = glGetUniformLocation(_shader.ID, "lightPos");
+	glUniform3f(lightPosLoc, lightX, 0.0f, 0.0f);
 	
 	glm::mat4 model = glm::mat4(1.0f);
 	
 	unsigned int modelLoc = glGetUniformLocation(_shader.ID, "model");
 	unsigned int viewLoc  = glGetUniformLocation(_shader.ID, "view");
 	unsigned int projLoc  = glGetUniformLocation(_shader.ID, "projection");
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(_camera.getView()));
 	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(_camera.getProjection()));
 	
 	// Render cube
-	glBindVertexArray(_vao);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	for (int i = 0; i < 10; i++)
+	{
+		model = glm::translate(model, glm::vec3(cos(i * 180) * 10, 0.0f, sin(i * 180) * 10));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glBindVertexArray(_vao);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 	
 	// Render light object, not the actual light
 	_lightShader.use();
@@ -111,7 +121,7 @@ void BasicLighting::update(int deltaX, int deltaY)
 	lightColorLoc = glGetUniformLocation(_lightShader.ID, "lightColor");
 	glUniform3f(lightColorLoc, lr, lg, lb);
 	glm::mat4 lightModel = glm::mat4(1.0f);
-	lightModel = glm::translate(lightModel, glm::vec3(2.0f, 0.0f, 0.0f));
+	lightModel = glm::translate(lightModel, glm::vec3(lightX, 0.0f, 0.0f));
 	modelLoc = glGetUniformLocation(_shader.ID, "model");
 	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(lightModel));
 	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(_camera.getView()));
